70c908c1377b33d65aa2fe854d887bf0c8b9afa4
farid ghaderi <farid.51gh@gmail.com>
2022-03-19 15:40:45 +0330

    write unit tests for user controller

--- fourth-project-cars-value/src/users/users.controller.spec.ts
+++ fourth-project-cars-value/src/users/users.controller.spec.ts
@@ -1,12 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { async } from 'rxjs';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUserService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUserService = {
+      findOne: (id: number) =>
+        Promise.resolve({
+          id,
+          email: 'test@test.com',
+          password: '123',
+        } as User),
+      find: (email: string) =>
+        Promise.resolve([{ id: 1, email, password: '123' } as User]),
+      // remove: (id:number) => Promise.resolve({id,email:"test@test.com",password:"2134"} as User),
+      // update: (id:number,attrs:Partial<User>) => Promise.resolve({id,...attrs} as User),
+    };
+    fakeAuthService = {
+      signup: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+      signin: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +49,34 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('test@test.com');
+    expect(users.length).toBe(1);
+    expect(users[0].email).toEqual('test@test.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if a user with the given id did not found', async () => {
+    fakeUserService.findOne = () => null;
+    try {
+      await controller.findUser('1');
+    } catch (error) {
+      return true;
+    }
+  });
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'test@test.com', password: '123456' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
--- fourth-project-cars-value/src/users/users.controller.ts
+++ fourth-project-cars-value/src/users/users.controller.ts
@@ -15,12 +15,12 @@ import {
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/Interceptors/serialize.interceptor';
+import { Serialize } from '../Interceptors/serialize.interceptor';
 import { UserDTO } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 @Controller('auth')
 @Serialize(UserDTO)
 export class UsersController {
